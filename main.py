1263e5cef6ce1a4f44921a4bd048de615a841581
gnnchya <75212443+gnnchya@users.noreply.github.com>
2022-04-10 21:48:46 +0700

    add sending request to the server

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask_login import login_user, login_required, current_user, logout_user, L
 import os
 import time
 import threading
+import requests
 
 template_dir = os.path.abspath('templates')
 static_dir = os.path.abspath('static')
@@ -13,7 +14,7 @@ login_manager.init_app(app)
 login_manager.session_protection = "strong"
 
 stop_threads = False
-
+lid = "03088ffc-1324-4ee1-ae59-a62c7fc63908"
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -117,21 +118,46 @@ def rfid_api():
     global stop_threads
 
     def open_locker(slot_no):
+        url = "http://127.0.0.1:8000/keptlock/locker/unlock/" + lid;
         timeout = time.time() + 60  # 1 minute
         if slot == "all":
             print("open all")
-            # TODO do something with the locker
+            status = [True, True, True]
             while True:
+                # TODO do something with the locker
                 print("open all")
                 if stop_threads or time.time() > timeout:
-                    break
+                    if stop_threads:
+                        r = requests.post(url, data=status)
+                        print(r.status_code, r.reason, r.content)
+                        url_vid = "http://127.0.0.1:8000/keptlock/locker/video"
+
+                        for i in range(3):
+                            files = {'vid1': open('PATHHHHHHHHHHH', 'rb'), 'vid2': open('PATHHHHHHHHHHH', 'rb')}
+                            data = {"lid": lid, "slot": i}
+                            r = requests.post(url_vid, data=data, files=files)
+                            print(r.status_code, r.reason, r.content)
+
+                        break
         else:
             print("open slot#", slot_no)
-            # TODO do something with the locker
+            status_now = [False, False, False]
+            status_now[slot_no-1] = True
+            status = {"slots": status_now}
             while True:
+                # TODO do something with the locker
                 print("open slot#", slot_no)
                 if stop_threads or time.time() > timeout:
-                    break
+                    if stop_threads:
+                        r = requests.post(url, data=status)
+                        print(r.status_code, r.reason, r.content)
+
+                        url_vid = "http://127.0.0.1:8000/keptlock/locker/video"
+                        files = {'vid1': open('PATHHHHHHHHHHH22', 'rb'), 'vid2': open('PATHHHHHHHHHHH', 'rb')}
+                        data = {"lid": lid, "slot": slot_no}
+                        r = requests.post(url_vid, data=data, files=files)
+                        print(r.status_code, r.reason, r.content)
+                        break
 
     # mock up data (UID of the locker account)
     uid = 12345678
@@ -185,15 +211,20 @@ def pin_api():
 
     if request.method == 'POST':
         pin = request.form['pin_enter']
-        # TODO check pin in the database and get slot no.
-        slot = 1
-        pin_exist = True
-        pin_expired = False
-        if not pin_exist:
-            flash("Invalid pin")
-            return redirect("http://127.0.0.1:8000/keptlock/pin")
-        elif pin_expired:
-            flash("Pin expired")
+
+        # TODO need testing
+        url = "http://127.0.0.1:8000/keptlock/locker/unlock/pin" + lid;
+        code = {'code': pin}
+        r = requests.post(url, data=code)
+        print(r.status_code, r.reason, r.content)
+
+        slot = int(r.content)
+        pin_valid = False
+        if r.status_code == 200:
+            pin_valid = True
+
+        if not pin_valid:
+            flash("Invalid pin or expired")
             return redirect("http://127.0.0.1:8000/keptlock/pin")
         else:
             print(pin, "open slot no.", slot)
